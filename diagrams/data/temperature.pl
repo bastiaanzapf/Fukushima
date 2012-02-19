0a5159334ccfe9d8c287203bcf0d6a44da1452dd
Bastiaan Zapf <bzapf@online.de>
2012-02-19 16:18:35 +0100

    Design

--- diagrams/data/temperature.pl
+++ diagrams/data/temperature.pl
@@ -7,10 +7,12 @@ $a=<>;
 open COMMANDS, ">commands.gpl" or die $!;
 open DATA, ">data" or die $!;
 
-print COMMANDS "set terminal jpeg xffffff x000000 size 1024,768\n";
-print COMMANDS "set object rectangle from screen 0,0 to screen 1,1 lw 0 fillstyle noborder behind\n";
+print COMMANDS "set terminal jpeg xffffff x000000 size 1200,768\n";
+print COMMANDS "set object rectangle from screen 0,0 to screen 1.1,1.1 lw 0 fillstyle noborder behind\n";
 print COMMANDS "set output \"../temp2.jpeg\"\n";
 print COMMANDS "set xdata time\n";
+print COMMANDS "set key left top\n";
+
 print COMMANDS "set timefmt \"%Y/%m/%d-%H:%M\"\n";
 print COMMANDS "set xrange [\"2012/02/01-00:00\":\"2012/02/20-00:00\"]\n";
 print COMMANDS "plot";
