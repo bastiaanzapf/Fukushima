e0a949e8b873ade80a8ab6ea6c6547d1df45a3e3
Bastiaan Zapf <bzapf@online.de>
2012-02-19 15:40:57 +0100

    gnuplot works

--- diagrams/data/temperature.pl
+++ diagrams/data/temperature.pl
@@ -7,14 +7,21 @@ $a=<>;
 open COMMANDS, ">commands.gpl" or die $!;
 open DATA, ">data" or die $!;
 
-print COMMANDS "set terminal jpeg\n";
+print COMMANDS "set terminal jpeg size 1024,768\n";
 print COMMANDS "set output \"../temp2.jpeg\"\n";
+print COMMANDS "set xdata time\n";
+print COMMANDS "set timefmt \"%Y/%m/%d-%H:%M\"\n";
+print COMMANDS "set xrange [\"2012/02/01-00:00\":\"2012/02/20-00:00\"]\n";
+print COMMANDS "plot";
 
 for my $i (1 .. $#headers) {
     $headers[$i] =~ s/\s+$//;
-    if ($headers[$i] ne "note") {
-	print COMMANDS "plot \"data\" using 1:".($i+1)." title \"$headers[$i]\"\n";
+    if ($i != 1 && $headers[$i] ne "note") {
+	print COMMANDS ", \\\n"
     }
+    if ($headers[$i] ne "note") {
+	print COMMANDS " \"data\" using 1:".($i+1)." title \"$headers[$i]\"";
+    }    
 }
 
 $i=0;
@@ -23,6 +30,9 @@ while (<>) {
 
     for (@d) {
 	$_ =~ s/\s+$//;
+	if ($_ =~ m/20..\//) { # if date, replace space with -
+	    $_ =~ s/ /-/;
+	}
 	print DATA $_."\t";
     }
     print DATA "\n";
